949672a79c1cd6236a2684885f0f410e0f3c5c96
fyodor91 <fyodorermakov91@gmail.com>
2023-04-04 22:26:56 +0700

    step 3.0.2

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,4 +3,4 @@ import prompt
 def welcome_user():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    return 'Hello, ' + name  + '!'
+    print('Hello, ' + name  + '!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import sys
+sys.path.insert(0, '/home/fyodor/python-project-49/brain_games')
+import cli
+
 def main():
-    print('Welcome to the Brain Games!')
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
