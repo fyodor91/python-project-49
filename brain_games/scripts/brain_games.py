bc3112358244291edc9c6883ca270ee7232effd1
fyodor91 <fyodorermakov91@gmail.com>
2023-04-15 12:39:44 +0700

    step 1

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-import sys
-sys.path.insert(0, '/home/fyodor/python-project-49/brain_games')
-from brain_games.cli import welcome_user
-
 
 def main():
-    welcome_user()
+    print('Welcome to the Brain Games!')
 
 
 if __name__ == '__main__':
