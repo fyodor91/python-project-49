e6d69271e03ddeb95029dd2bc40ea1952d72112c
fyodor91 <fyodorermakov91@gmail.com>
2023-04-16 14:52:08 +0700

    step 4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 
-import sys
-sys.path.insert(0, '/home/fyodor/python-project-49/brain_games')
-from cli import welcome_user
+from brain_games.cli import welcome_user
 
 
 def main():
